7469e946a83273c7f63963574eb35ed43b202b6c
Fernandes072 <joaofilho8274@gmail.com>
2023-08-11 10:34:26 -0300

    XadrezException e PosicaoXadrez

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,6 +1,5 @@
 package xadrez;
 
-import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -24,9 +23,13 @@ public class PartidaXadrez {
 		return mat;
 	}
 	
+	private void colocarNovaPeca(char coluna, int linha, PecaXadrez peca) {
+		tabuleiro.lugarPeca(peca, new PosicaoXadrez(coluna, linha).toPosicao());
+	}
+	
 	private void configuracaoInicial() {
-		tabuleiro.lugarPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao (2,1));
-		tabuleiro.lugarPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(0,4));
-		tabuleiro.lugarPeca(new Rei(tabuleiro, Cor.BRANCO), new Posicao(7,4));
+		colocarNovaPeca('B', 6, new Torre(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('E', 8, new Rei(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('E', 1, new Rei(tabuleiro, Cor.BRANCO));
 	}
 }
