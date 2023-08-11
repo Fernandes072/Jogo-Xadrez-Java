206e0d3d277ca109559c19d8a026f10509c99e66
Fernandes072 <joaofilho8274@gmail.com>
2023-08-11 16:58:30 -0300

    Manipular exceções e limpar a tela

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,12 @@
 package aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
+import xadrez.XadrezException;
 
 public class Programa {
 	
@@ -14,6 +16,8 @@ public class Programa {
 		PartidaXadrez partida = new PartidaXadrez();
 		
 		while (true) {
+			try {
+			UI.limparTela();
 			UI.exibirTabuleiro(partida.getPecas());
 			
 			System.out.println();
@@ -22,6 +26,15 @@ public class Programa {
 			
 			PecaXadrez pecaCapturada = partida.executarJogada(jogada[0], jogada[1]);
 			System.out.println();
+			} catch (XadrezException e) {
+				System.out.println(e.getMessage());
+				System.out.print("Pressione Enter para continuar!");
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				System.out.print("Pressione Enter para continuar!");
+				sc.nextLine();
+			}
 		}
 		
 	}
